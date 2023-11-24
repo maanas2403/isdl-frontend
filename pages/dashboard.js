715a821f9cad13fa045631a809f82e43f77d2221
maanas2403 <108995876+maanas2403@users.noreply.github.com>
2023-11-24 21:51:29 +0530

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -83,7 +83,7 @@ function dashboard() {
     } else {
       (async () => {
         const response = await fetch(
-          "https://isdl-backend-3-q6qqa0oc9-maanas2403.vercel.app/getUserBookings?" +
+          "https://isdl-backend-2-5q8zq3tkb-maanas2403.vercel.app/getUserBookings?" +
             new URLSearchParams({ jwt: Jwt }),
           {
             method: "GET",
