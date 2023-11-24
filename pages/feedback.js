ef21574b6d9d382a9823cc02914bbeb218636ab0
maanas2403 <108995876+maanas2403@users.noreply.github.com>
2023-11-24 22:00:20 +0530

    Update feedback.js

--- pages/feedback.js
+++ pages/feedback.js
@@ -13,7 +13,7 @@ function feedback() {
   async function sendFeedback(e) {
     e.preventDefault();
       const response = await fetch(
-        "https://isdl-backend-2-5q8zq3tkb-maanas2403.vercel.app/sendFeedback?",
+        "http://isdl-backend-2-maanas2403.vercel.app/sendFeedback?",
         {
           method: "POST",
           headers: {
