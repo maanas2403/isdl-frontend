c03eb478acdea580f41e9036e4171b758f0f00d9
maanas2403 <108995876+maanas2403@users.noreply.github.com>
2023-11-25 17:15:31 +0530

    Update halls.js

--- pages/halls.js
+++ pages/halls.js
@@ -108,7 +108,7 @@ const halls = () => {
     );
   } else {
     (async () => {
-      const response = await fetch("http://isdl-backend-2-maanas2403.vercel.app/allHalls?", {
+      const response = await fetch("https://isdl-backend-2-maanas2403.vercel.app/allHalls?", {
         method: "GET",
       });
       let data = await response.json();
